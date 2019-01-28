70764bf3b55194aa92778e402cebba74c1611d4f
TarekSherif <ENG.TAREK.SHERIF@gmail.com>
2019-01-28 17:40:01 +0200

    v1_Auth_Register

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,22 @@ Route::group([  'prefix' => 'admin', 'as' => 'admin.'], function () {
 
 Route::get('ListOfACEmp', 'Admin\EmployeesController@ListOfACEmp');
 
-
-Auth::routes();
-
-
-
 Route::get('lang/{lang}',  function($lang)
 {
     session()->has("lang")?session()->forget("lang"):"";
     $lang=="ar"?  session()->put("lang","ar"):  session()->put("lang","en");
     return back();
 });
+
+
+ // Authentication Routes...
+ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+ Route::post('login', 'Auth\LoginController@login');
+ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+ 
+ // Password Reset Routes...
+ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
